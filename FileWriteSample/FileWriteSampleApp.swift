8ac72f596611444f7768abba0617dfa94787f3e7
Haruhito Fuji <haruhito.fuji@gmail.com>
2023-01-09 19:43:28 +0900

    新規作成

---
+++ FileWriteSample/FileWriteSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileWriteSampleApp.swift
+//  FileWriteSample
+//
+//  Created by 藤治仁 on 2023/01/09.
+//
+
+import SwiftUI
+
+@main
+struct FileWriteSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
